4287007efb2e80b46c14cd272fbe1164ab991895
collins <ugwucollins64@gmail.com>
2019-12-09 11:05:29 +0100

    fix: remember token assigned to user correctly

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,8 @@
 
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token] = User.new_token
+    remember_token = User.new_token
+    cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
     current_user
   end
